36bdbd976ed0b1a2cb95c7a17de33e93d9ad6978
Rex Hsu <aexz5476@gmail.com>
2023-08-16 15:20:30 +0800

    build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
+// 載入模組（Express、Handlebars)、設定路由
+
 // 載入 Express
 const express = require('express')
+// 載入樣版引擎 express-handlebars 
+const { engine } = require('express-handlebars')
 // 執行 Express function，建立 app
 const app = express()
 // 指定 port
 const port = 3000
 
+// 把樣板引擎交給 express-handlebars，名稱為參數 .hbs
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+// 將 public 裡的檔案載入 Express 中
+app.use(express.static('public'))
+
 // 設定路由(根目錄)
-// 網站的首頁會直接導向(redirect) restaurants 清單
+// 將網站的首頁直接導向(redirect) restaurants 清單
+// 將靜態檔案的路徑 /restaurants 提供給 app.use
 app.get('/', (req, res) => {
-  res.redirect('express app for restaurants')
+  res.redirect('/restaurants')
 })
 
+// res.send('listing movies') 改成 res.render('index')
 app.get('/restaurants', (req, res) => {
-  res.send('listing restaurants')
+  res.render('index')
 })
 
 // 使用 params 做動態路由
